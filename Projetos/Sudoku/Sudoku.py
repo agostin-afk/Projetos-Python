ec07b7caa54abc5c7875b97f35188d96f11c83d8
agostin-afk <agostinho008.af45@gmail.com>
2023-03-24 22:16:15 -0300

    melhorias

--- Projetos/Sudoku/Sudoku.py
+++ Projetos/Sudoku/Sudoku.py
@@ -10,7 +10,7 @@ def montarTabela():
     return tabela
 
 
-def exibir_blocos(tabela, tamanho=2):
+def blocos_separados(tabela, tamanho=2):
     bloco = []
     for linha in range(0, len(tabela), tamanho):
         for coluna in range(0, len(tabela[0]), tamanho):
@@ -18,39 +18,43 @@ def exibir_blocos(tabela, tamanho=2):
             for i in range(tamanho):
                 for j in range(tamanho):
                     if coluna + j < len(tabela[0]):
-                        print(f'{tabela[linha+i][coluna+j]:^4}', end=' ')
+                        #print(f'{tabela[linha+i][coluna+j]:^4}', end=' ')
                         x = tabela[linha+i][coluna+j]
                         aux.append(x)
-                print()
+                #print()
             bloco.append(aux[:])
-            print('-'*30)
-    print(bloco)
+            #print('-'*30)
+    #print(bloco)
     return bloco
-def linhas_separadas(t):
+
+
+def linhas_separadas(tabela):
     linhas = []
-    for linha in range(0, len(t)):
+    '''for linha in range(0, len(tabela)):
         aux = []
-        for coluna in range(0, len(t)):
-            aux.append(t[linha][coluna])
-        linhas.append(aux[:])
-    print(linhas)
-def colunas_separadas(t):
+        for coluna in range(0, len(tabela)):
+            aux.append(tabela[linha][coluna])
+        linhas.append(aux[:])'''
+    linhas = tabela[:]
+    return linhas
+
+
+def colunas_separadas(tabela):
     colunas = []
-    for coluna in range(0, len(t)):
+    for coluna in range(0, len(tabela)):
         aux = []
-        for linha in range(0, len(t)):
-            aux.append(t[linha][coluna])
+        for linha in range(0, len(tabela)):
+            aux.append(tabela[linha][coluna])
         colunas.append(aux[:])
-    print(colunas)
-def checagem_bloco(bloco):
-    bloco_repeticao = []
-    for x in range(0, 3):
-        tem_repetidos = len(bloco[x]) != len(set(bloco[x]))
-        if tem_repetidos:
-            bloco_repeticao.append(x+1)
-    return(bloco_repeticao)
-t = montarTabela()
-p = exibir_blocos(t)
-print(checagem_bloco(p))
-linhas_separadas(t)
-colunas_separadas(t)
+    return colunas
+
+
+def conferir(blocos, linhas, colunas):
+    pass
+
+
+tabela = montarTabela()
+blocos = blocos_separados(tabela)
+linhas = linhas_separadas(tabela)
+colunas = colunas_separadas(tabela)
+print(f'{tabela}\n{blocos}\n{linhas}\n{colunas}')
