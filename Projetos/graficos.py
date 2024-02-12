48ceddf18c876c133d33df3c5068f589afc843f9
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-02-11 21:42:20 -0300

    Update graficos.py

--- Projetos/graficos.py
+++ Projetos/graficos.py
@@ -1,10 +1,13 @@
+""" 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
 figure, axes = plt.subplots(10, 10)
 circle = plt.Circle((0.6, 0.6), 0.3, fill=False)
-axes.set_aspect(1)
+axes.set_aspect(1) 
 axes.add_artist(circle)
 plt.title('Teste')
 plt.show()
+
+"""
