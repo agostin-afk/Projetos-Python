4eb276aba2ca1d0624d2de4ee3bcd7739343d6ca
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-04-18 09:57:06 -0300

    Create dict_aleatorio.py

---
+++ Projetos/Cadeia_markov/dict_aleatorio.py
@@ -0,0 +1,18 @@
+import random
+
+pesos = [90, 10]
+
+dict_teste = {
+    'Agosto': 'Ferreira',
+    'Ferreira': 'Dale papai',
+              }
+#for k,v in dict_teste.items():
+#    print(f'{k} -> {v}')
+    
+dici_dict = {
+    "agosto": {"ferreira": pesos[0]},
+    "Ferreira": {"Dale papai": pesos[1]},
+}
+for k, i in dici_dict.items():
+    print(f'{k} -> {list(i.keys())[0]} -> {int(list(i.values())[0])}')
+    
